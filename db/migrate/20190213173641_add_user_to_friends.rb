5475ad878528ebf77615c4c7f421137c7f4fd110
Cortezz <jospcortez@gmail.com>
2019-02-13 17:37:21 +0000

    Add user reference to friends

---
+++ db/migrate/20190213173641_add_user_to_friends.rb
@@ -0,0 +1,5 @@
+class AddUserToFriends < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :friends, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_173118) do
+ActiveRecord::Schema.define(version: 2019_02_13_173641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_13_173118) do
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_02_13_173118) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "friends", "users"
 end
